2d3ee58b4b1bbd4b7012cb449cacb644b98a2eb5
Kasun Gamage <kasun.gamage@webstasy.one>
2017-09-30 19:34:45 +0530

    chaos control by user input. Resolves #4 . Binary Matrix added. Resolves #6 .

--- heu.c
+++ heu.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
-#include <math.h>
 
-int main(){
-	int i;
+void randomStarSpace();
+void binaryMatrix();
+
+int main()
+{
+    int option;
+	printf("Chaos 1. * and space2. Binary Matrix");
+	scanf("%i", &option);
+    int i;
+    switch(option){
+        case 1 : randomStarSpace();
+            break;
+		case 2 : binaryMatrix();
+			break;
+		default : printf("Invalid option");
+    }
+}
+
+void randomStarSpace(){
+    int i;
 	for(i = 0; ; i++){
 		int r = rand() % 2;
 		printf("%c", r == 1 ? '*' : ' ');
+	}
+}
+
+void binaryMatrix(){
+    int i;
+	for(i = 0; ; i++){
+		int r = rand() % 2;
+		printf("%c", r == 0 ? '0' : '1');
 	}
 }
